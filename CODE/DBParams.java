ab6fbae969b992570958514ebb557152ff8a091b
imanehib <imane.hibaoui23@gmail.com>
2023-09-28 14:59:49 +0100

    Creation de la classe Main et DBParams

---
+++ CODE/DBParams.java
@@ -0,0 +1,8 @@
+public class DBParams {
+    public static String DBPath;
+    public static int SGBDPageSize;
+    public static int DMFileCount;
+    
+
+}
+
---
+++ CODE/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        DBParams.DBPath = args[0];
+        DBParams.SGBDPageSize = 4096 ;
+        DDParams.DMFileCount = 4;
+
+
+        
+    }
+
+}
