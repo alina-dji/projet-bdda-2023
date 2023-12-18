1b5046b0faeda0c15ac0d5d8f47c7d38ee343163
imanehib <145684080+imanehib@users.noreply.github.com>
2023-12-19 00:32:28 +0100

    Maj Frame

--- CODE/MAIN/Frame.java
+++ CODE/MAIN/Frame.java
@@ -1,23 +1,56 @@
 package Main;
 
-public class Frame {
+import java.nio.ByteBuffer;
 
-    public byte[] buffer;
-    public PageId pageId;
-    public int pinCount;
-    public boolean dirty;
+public class Frame {
+    private PageId pageId;
+    private ByteBuffer buffer;
+    private int pinCount;
+    private boolean dirty;
     public int accessCount;
 
     public Frame() {
-        buffer = new byte[DBParams.SGBDPageSize];
-        pageId = null;
-        pinCount = 0;
-        dirty = false;
-        accessCount = 0;
+        this.pageId = new PageId(-1, 0);
+        this.buffer = ByteBuffer.allocate(DBParams.SGBDPageSize);
+        this.pinCount = 0;
+        this.dirty = false;
+        this.accessCount = 0;
     }
-    
-    public void incrementAccessCount() {
-        accessCount++;
+
+    public PageId getPageId() {
+        return pageId;
+    }
+
+    public ByteBuffer getBuffer() {
+        return buffer;
+    }
+
+    public int getPinCount() {
+        return pinCount;
+    }
+
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    public int getAccessCount() {
+        return accessCount;
+    }
+
+    public void setPageId(PageId pageId) {
+        this.pageId = pageId;
+    }
+
+    public void setBuffer(ByteBuffer buffer) {
+        this.buffer = buffer;
+    }
+
+    public void setPinCount(int pinCount) {
+        this.pinCount = pinCount;
+    }
+
+    public void setDirty(boolean dirty) {
+        this.dirty = dirty;
     }
 
     public void incrementPinCount() {
@@ -28,12 +61,24 @@ public class Frame {
         pinCount--;
     }
 
+    public void incrementAccessCount() {
+        accessCount++;
+    }
+
     public void reset() {
-        this.buffer = null;
-        this.pageId = null;
+        this.pageId = new PageId(-1, 0);
+        this.buffer.clear();
         this.pinCount = 0;
         this.dirty = false;
         this.accessCount = 0;
     }
 
+    @Override
+    public String toString() {
+        return "Frame{" +
+                "pageId=" + pageId +
+                ", pinCount=" + pinCount +
+                ", dirty=" + dirty +
+                '}';
+    }
 }
